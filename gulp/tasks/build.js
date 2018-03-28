0d4b2d08bb40b1dfb6b559ecef74e4e233816b3d
litor <547794529@qq.com>
2018-03-28 13:38:08 +0800

    modify task for gulp4

--- gulp/tasks/build.js
+++ gulp/tasks/build.js
@@ -15,56 +15,65 @@ import env from 'gulp-env'
 var envs = {NODE_ENV: config.NODE_ENV}
 
 gulp.task('webpack', gulp.series(function (cb) {
-    return gulp
-        .src([__dirname+'/index.js'])
-        .pipe(env.set(envs))
-        .pipe(errorHandler())
-        .pipe(named())
-        .pipe(webpackGulp(configWebpack))
-        .pipe(gulp.dest(config.argDist || config.dest))
-        .on('end', function(){
-            cb()
-            if(config.isProduction){
-                process.exit()
-            }
-        })
+  cb()
+  return gulp
+    .src([__dirname+'/index.js'])
+    .pipe(env.set(envs))
+    .pipe(errorHandler())
+    .pipe(named())
+    .pipe(webpackGulp(configWebpack))
+    .pipe(gulp.dest(config.argDist || config.dest))
+    .pipe(connect.reload())
+    .on('end', function(){
+      cb()
+      if(config.isProduction){
+        process.exit()
+      }
+    })
 }))
 
-gulp.task('connect', function(){
-    connect.server({
-        root: config.dest,
-        port: config.server.port || '8081',
-        livereload: true,
-        middleware: function (connect, opt) {
-            let proxys = []
 
-            if (config.proxy) {
-                for (let i = 0; i < config.proxy.length; i++) {
-                    proxys.push(proxy(config.proxy[i].source, {
-                        target: config.proxy[i].target,
-                        changeOrigin: true,
-                        secure: false,
-                        headers: {
-                            Connection: 'keep-alive'
-                        }
-                    }))
-                }
+gulp.task('connect', function(cb){
+  connect.server({
+    root: config.dest,
+    port: config.server.port || '8081',
+    livereload: true,
+    middleware: function (connect, opt) {
+      let proxys = []
+
+      if (config.proxy) {
+        for (let i = 0; i < config.proxy.length; i++) {
+          proxys.push(proxy(config.proxy[i].source, {
+            target: config.proxy[i].target,
+            changeOrigin: true,
+            secure: false,
+            headers: {
+              Connection: 'keep-alive'
             }
-
-            return proxys
+          }))
         }
-    })
-})
+      }
 
-gulp.task('clean', function(){
-    var dest = config.dest
-    try {
-        del.sync([dest + '/**/*', '!' + dest + '/WEB-INF/**', '!' + dest + '/APP_INFO/**'], {force: true})
-    } catch (e) {
-        console.log('%s do not clean', dest)
+      return proxys
     }
+  })
+  cb()
 })
 
-gulp.task('build', gulp.series('clean', 'webpack'))
+gulp.task('clean', function(cb){
+  var dest = config.dest
+  try {
+    del.sync([dest + '/**/*', '!' + dest + '/WEB-INF/**', '!' + dest + '/APP_INFO/**'], {force: true})
+  } catch (e) {
+    console.log('%s do not clean', dest)
+  }
+  cb()
+})
 
-gulp.task('default', gulp.series('clean', 'webpack', 'connect'))
+gulp.task('build', gulp.series(gulp.parallel('clean', 'webpack'),function(){
+
+}))
+
+gulp.task('default', gulp.series('clean',gulp.parallel( 'webpack', 'connect'),function(cb){
+  cb()
+}))
